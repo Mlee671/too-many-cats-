512a4188569af3d131a99b52d2331fccf361cbd1
Crynner <calvin.shih19@gmail.com>
2026-04-05 09:05:18 +1200

    adjusted wander pattern, added wander stall clock, fixed null-read bug (terribly)

--- entity/enemy/enemy.gd
+++ entity/enemy/enemy.gd
@@ -6,44 +6,64 @@ class_name Enemy
 @export var move_speed: float
 var accel: int
 
+enum BEHAVIOUR {WANDER, ATTACK}
+
+var enemyState := BEHAVIOUR.WANDER
+var wander_stalling := false
+
 #move
 #nav
 #proc on char sight
 #hp signals
 #
 
-@onready var nav_agent: NavigationAgent2D = $NavigationAgent2D
+@onready var wander_timer := $wander_stall_timer
+@onready var nav_agent := $NavigationAgent2D
 
-func _ready():
-	# Performance tip: Don't update the target every frame if it doesn't move.
-	# We wait for the first physics frame so the NavigationServer is ready.
-	call_deferred("setup_nav")
+func nearby_vector(range: Vector2) -> Vector2i:
+	return Vector2i(
+		randi_range(position.x - range.x, position.x + range.x),
+		randi_range(position.y - range.y, position.y + range.y))
 
-func setup_nav():
-	# Wait for the first physics frame to ensure the map is synced
+func _ready() -> void:
+	setup_nav.call_deferred()
+
+func setup_nav() -> void:
+	while tilemap == null:
+		await get_tree().process_frame
+	# wait until map sync
 	await get_tree().physics_frame
-	set_movement_target() # Initialize at current spot
+	set_wander_target()
 
-func set_movement_target():
-	nav_agent.target_position = _get_new_pos()
-# 14,2 24,10
-func _get_new_pos():
-	return tilemap.map_to_local(Vector2i(randi_range(14,39),randi_range(2,21)))
+func set_wander_target() -> void:
+	nav_agent.target_position = nearby_vector(tilemap.map_to_local(Vector2i(6, 6)))
+	print(nav_agent.target_position)
 
-func _physics_process(_delta):
-	if nav_agent.is_navigation_finished():
-		set_movement_target()
+# empty function to fill with attack behaviour for subclasses
+func attack_logic() -> void:
+	push_error("Attack logic for enemy not implemented, must be overwritten.")
 
-	# 1. Find the next point in the path
-	var current_agent_position:= global_position
-	var next_path_position:= nav_agent.get_next_path_position()
+func _physics_process(_delta) -> void:
+	if enemyState == BEHAVIOUR.WANDER and not wander_stalling:
+		# if at target node, get new target node
+		if nav_agent.is_navigation_finished():
+			wander_stalling = true
+			wander_timer.start(2)
 
-	# 2. get direction, multiply by movement scalar
-	var new_velocity:= next_path_position - current_agent_position
-	new_velocity = new_velocity.normalized() * move_speed
+		# get new direction to get to next path node
+		var new_velocity: Vector2 = (nav_agent.get_next_path_position() - global_position).normalized() * move_speed
+		nav_agent.set_velocity(new_velocity)
+	
+	# if attacking user, run corresponding logic
+	elif enemyState == BEHAVIOUR.ATTACK:
+		attack_logic()
 
-	nav_agent.set_velocity(new_velocity)
-
+## navigation agent handles movement after adjusting vector
 func _on_nav_dist_adjust(safe_velocity: Vector2) -> void:
 	velocity = safe_velocity
 	move_and_slide()
+
+
+func _on_wander_timeout() -> void:
+	wander_stalling = false
+	set_wander_target()
