f52bfd88301e63a3959d7673401c009fabb57342
Crynner <calvin.shih19@gmail.com>
2026-04-08 15:59:40 +1200

    projectile enforces lifespan of 10s (starts timer)

--- entities/enemy/components/attacks/projectile.gd
+++ entities/enemy/components/attacks/projectile.gd
@@ -8,7 +8,7 @@ var damage := 10
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	lifespan.start(10)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
