a61c12519f964e9644108570dbe08e235133a688
Mlee671 <mitchwill.lee@gmail.com>
2026-04-09 16:00:46 +1200

    Feat: rooms now have a list of all enemies inside of them and can activate once player gets close enough. enemy activation not implemented yet.

--- Room_scenes/room_template.gd
+++ Room_scenes/room_template.gd
@@ -6,6 +6,7 @@ const NORTH_DOOR_CLOSED := [Vector2i(6,3),Vector2i(7,3)]
 const SOUTH_DOOR_CLOSED := [Vector2i(6,6),Vector2i(7,6)]
 const NS_DOOR_OPEN := [Vector2i(7,5),Vector2i(8,5)]
 
+var enemy_list : Array = []
 
 @onready var objects := $NavRegion/Environment
 
@@ -21,13 +22,11 @@ func _on_west_door_body_exited(body: Node2D) -> void:
 		for i in 2:
 			objects.set_cell(doorway[i],0,WEST_DOOR[i],0)
 
-
 func _on_east_door_body_entered(body: Node2D) -> void:
 	if body is main_character:
 		var doorway := [Vector2i(9,-1), Vector2i(9,0)]
 		for i in 2:
 			objects.set_cell(doorway[i],0,EAST_DOOR[i],1)
-
 
 func _on_east_door_body_exited(body: Node2D) -> void:
 	if body is main_character:
@@ -35,13 +34,11 @@ func _on_east_door_body_exited(body: Node2D) -> void:
 		for i in 2:
 			objects.set_cell(doorway[i],0,EAST_DOOR[i],0)
 
-
 func _on_north_door_body_entered(body: Node2D) -> void:
 	if body is main_character:
 		var doorway := [Vector2i(-1,-10), Vector2i(0,-10)]
 		for i in 2:
 			objects.set_cell(doorway[i],0,NS_DOOR_OPEN[i],0)
-
 
 func _on_north_door_body_exited(body: Node2D) -> void:
 	if body is main_character:
@@ -49,16 +46,35 @@ func _on_north_door_body_exited(body: Node2D) -> void:
 		for i in 2:
 			objects.set_cell(doorway[i],0,NORTH_DOOR_CLOSED[i],0)
 
-
 func _on_south_door_body_entered(body: Node2D) -> void:
 	if body is main_character:
 		var doorway := [Vector2i(-1,9), Vector2i(0,9)]
 		for i in 2:
 			objects.set_cell(doorway[i],0,NS_DOOR_OPEN[i],0)
 
-
 func _on_south_door_body_exited(body: Node2D) -> void:
 	if body is main_character:
 		var doorway := [Vector2i(-1,9), Vector2i(0,9)]
 		for i in 2:
 			objects.set_cell(doorway[i],0,SOUTH_DOOR_CLOSED[i],0)
+
+func _on_room_activator_body_entered(body: Node2D) -> void:
+	if body is Enemy:
+		# print("enemy added")
+		enemy_list.append(body)
+	elif body is main_character:
+		for enemy in enemy_list:
+			# print("enemies activated")
+			# activation method here
+			pass
+
+func _on_room_activator_body_exited(body: Node2D) -> void:
+	if body is main_character:
+		for i in range(enemy_list.size() - 1, -1, -1):
+			if enemy_list[i]:
+				# print("enemies deactivated")
+				# deactivate here. though once we have combat the doors will be locked
+				pass
+			else:
+				enemy_list.remove_at(i)
+				# print("removed dead enemy")
