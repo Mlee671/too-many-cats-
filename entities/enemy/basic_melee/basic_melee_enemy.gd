27112bbc4a06bfea8b779fc505207be423cb5ec0
Crynner <calvin.shih19@gmail.com>
2026-04-11 16:00:03 +1200

    refactored attack to orbiting area 2d nodes around the enemy for detection and hit-checking

--- entities/enemy/basic_melee/basic_melee_enemy.gd
+++ entities/enemy/basic_melee/basic_melee_enemy.gd
@@ -1,10 +1,8 @@
 extends Enemy
 class_name BasicMeleeEnemy
 
-## The distance to the player where the enemy will trigger their attack
-const ATTACK_PROC_DISTANCE := 27.0
+const ATTACK_DURATION := 0.5
 
-@export var melee := preload("res://entities/enemy/components/attacks/slash/slash.tscn") 
 @export var speed := 50.0
 @export var acceleration := 20.0
 @export var wander_range := 6
@@ -14,39 +12,60 @@ const ATTACK_PROC_DISTANCE := 27.0
 ## The distance the melee attack spawns from the enemy position
 @export var attack_offset_magnitude := 20.0
 
+@onready var attack_visual := $AttackComponent
+
+@onready var detect_radius := $AttackComponent/AttackDetect
+@onready var attack_box := $AttackComponent/Attackbox
+@onready var attack_sprite := $AttackComponent/AttackSprite
+@onready var attack_duration_timer := $AttackRenderTimer
+
 
 var frame : int = 0
-var current_attack : Slash = null
-var attack_offset : Vector2
 
 func _ready() -> void:
 	move_speed = speed
 	accel = acceleration
 	pathfind_range = wander_range
 	health.set_health(hp)
+	
+	attack_box.monitoring = false
+	attack_sprite.visible = false
 	super._ready()
 
+
+func _physics_process(delta: float) -> void:
+	if attack_box.monitoring and raycast_target and not attack_cooldown:
+		attack_visual.look_at(raycast_target.global_position)
+	super._physics_process(delta)
+
+
 func attack_logic() -> void:
-	frame += 1
-
-	var player_enemy_vec := global_position.direction_to(raycast_target.global_position)
-	var player_enemy_dist := global_position.distance_squared_to(raycast_target.global_position)
-
 	# move to player directly, no need to trigger every frame
 	if (frame % 10 == 0):
 		nav_agent.target_position = raycast_target.global_position
+	frame += 1
+
+
+func _on_attack_radius_triggered(_body: Node2D) -> void:
+	attack_box.monitoring = true
 	
-	if not attack_cooldown and player_enemy_dist < ATTACK_PROC_DISTANCE ** 2:
-		animation.play_animation("attack", true)
-		current_attack = melee.instantiate()
-		# point toward player
-		current_attack.global_position = global_position
-		current_attack.look_at(raycast_target.global_position)
-		attack_offset = player_enemy_vec * attack_offset_magnitude
-		current_attack.global_position += attack_offset
-		# put attack on cooldown, based on inverse attack rate (higher val = lower cd)
-		get_tree().root.add_child(current_attack)
-		attack_cooldown = true
-		stop_moving = true
-		nav_agent.set_velocity(Vector2.ZERO)
-		attack_timer.start(1.0 / attack_rate)
+	
+func _on_attack_radius_exited(_body: Node2D) -> void:
+	attack_box.monitoring = false
+
+
+func _on_attackbox_entered(body: Node2D) -> void:
+	attack_sprite.visible = true
+	animation.play_animation("attack", true)
+	body.take_damage(10)
+	attack_cooldown = true
+	stop_moving = true
+	nav_agent.set_velocity(Vector2.ZERO)
+	attack_timer.start(1.0 / attack_rate)
+	attack_duration_timer.start(ATTACK_DURATION)
+	
+
+# disables attack visuals, rotate hitbox so that body_entered can retrigger
+func _on_attack_render_timeout() -> void:
+	attack_sprite.visible = false
+	attack_visual.rotate(PI)
