2c357307dafdee528f52c3ac2acbc69170d9eb55
Jason Huang <124568457+Shio-y@users.noreply.github.com>
2026-04-18 23:04:32 +1200

    added main menu with functionality but all placeholder assets

--- main/main.gd
+++ main/main.gd
@@ -2,9 +2,20 @@ extends Node2D
 
 @onready var cm:=$character_manager
 @onready var room_manager := $Room_manager
+@export var character_hud: CanvasLayer
+
 
 
 func _ready() -> void:
+	#finishes the fading transition
+	$fade_transition.show()
+	$fade_transition/fade_transition_timer.start()
+	$fade_transition/AnimationPlayer.play("fade_out")
+	
 	# cm.switch_to("blue_knight")
 	room_manager.generate_rooms(5)
 	cm.spawn_character(Vector2(32,32))
+
+
+func _on_fade_transition_timer_timeout() -> void:
+	character_hud.visible = true
---
+++ main_menu/main_menu.gd
@@ -0,0 +1,45 @@
+extends Control
+@export var controls_image: TextureRect
+@export var background_image: TextureRect
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+#plays the fade in transition which will then change scene to main
+func _on_start_button_pressed() -> void:
+	
+	$fade_transition.show()
+	$fade_transition/fade_transition_timer.start()
+	$fade_transition/AnimationPlayer.play("fade_in")
+
+func _on_controls_button_pressed() -> void:
+	controls_image.visible = true
+	
+	
+func _on_quit_button_pressed() -> void:
+	get_tree().quit()
+
+#removes the controls image when you left click anywhere
+func _on_mouse_click(event: InputEvent) -> void:
+	if event.is_action_pressed("fire_gun"):
+		controls_image.visible = false
+
+#when you finish fading in, loads and switches to main
+func _on_fade_transition_timer_timeout() -> void:
+	
+	
+	
+	var scene : PackedScene = load("res://main/main.tscn")
+	
+	await get_tree().create_timer(1.2).timeout
+	await get_tree().process_frame
+	get_tree().change_scene_to_packed(scene)
+	
+	
