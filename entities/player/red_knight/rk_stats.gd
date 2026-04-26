5e918435dc499da163ff9254793cb722d78535ca
Crynner <calvin.shih19@gmail.com>
2026-04-27 09:54:07 +1200

    implemented basic red knight with quick firing.

---
+++ entities/player/red_knight/rk_stats.gd
@@ -0,0 +1,17 @@
+extends Stats
+class_name rk_state
+
+func _ready() -> void:
+	speed = 100
+	accel = 10 # how smooth stop/start movement
+	hp = 100
+	
+	evade_movement_scaling = 1.75
+	evade_dur = 0.4
+	evade_cd = 0.5
+
+	fire_cd =  0.1
+	ability_cd = 1
+	
+	projectile_frame = 1
+	projectile_speed = 250
