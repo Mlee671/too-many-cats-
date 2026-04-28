e25d8e89080e273934c787e41d9b227665e50661
Jason Huang <124568457+Shio-y@users.noreply.github.com>
2026-04-28 20:29:30 +1200

    added buttons to reward screen that randomly display a character

--- ui_items/reward_screen.gd
+++ ui_items/reward_screen.gd
@@ -1,12 +1,32 @@
 extends CanvasLayer
+# the scene name of each character
+enum characters {blue_knight, yellow_knight, red_knight,pink_knight}
 
+@onready var new_char_button: TextureButton = $Control/TextureRect/new_char_button
+
+var path := "res://entities/player/character_scenes/"
+var menu_path := "res://ui_assets/character_menu_sprite/"
+
+
+
+var chars_remaining = []
+var current_char 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	for c in characters:
+		chars_remaining.append(c)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	#opens the reward screen and picks a random character that hasnt been picked yet
 	if Input.is_action_just_pressed("debug_open_reward_screen"):
 		self.show()
+		current_char = chars_remaining.pick_random()
+		var char_menu : CompressedTexture2D = load(menu_path + current_char + "_menu.png")
+		new_char_button.texture_normal = char_menu
+
+
+func _on_new_char_button_pressed() -> void:
+	self.hide()
