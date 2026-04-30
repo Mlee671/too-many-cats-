7170d321ecb250d41fba9880c13e87571ff6455f
Crynner <calvin.shih19@gmail.com>
2026-04-30 21:24:48 +1200

    implemented proof of concept for bouncing bullets, sometimes clips out of bounds

--- entities/enemy/boss/boss_enemy.gd
+++ entities/enemy/boss/boss_enemy.gd
@@ -1,7 +1,7 @@
 extends Enemy
 class_name BossEnemy
 
-const PROJECTILE := preload("res://entities/enemy/components/projectile/enemy_projectile.tscn") 
+const PROJECTILE := preload("res://entities/enemy/boss/bouncing_proj.tscn") 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -18,13 +18,13 @@ func attack_logic() -> void:
 		return
 	if randi() % 2:
 		print("going")
-		for angle in [-30, 0, 30]:
+		for angle in [-10, -5, 5, 10]:
 			var proj := PROJECTILE.instantiate()
 			get_parent().add_child(proj)
 			proj.global_position = global_position
 			proj.set_velocity(100 * (raycast_target.global_position - proj.global_position).normalized().rotated(deg_to_rad(angle)))
 			
 			attack_cooldown = true
-			attack_timer.start(1.0 / 2)
+			attack_timer.start(1.0 / 3)
 	else:
 		pass
