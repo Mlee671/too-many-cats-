da3f128fadcc7bb36c4d345f094d6b7efffbc50c
Alastair Furey <afur834@aucklanduni.ac.nz>
2026-05-02 22:10:01 +1200

    feat: updated melee cat attack animation, imported 8 bit music

--- entities/player/blue_knight/blue_knight.gd
+++ entities/player/blue_knight/blue_knight.gd
@@ -7,7 +7,6 @@ class_name blue_knight
 
 func character_ability():
 	super()
-	print(stats.ability_dur/2.0)
 	await get_tree().create_timer(stats.ability_dur/2.0).timeout
 	nav_agent.target_position = get_global_mouse_position()
 	# go to final calculated path node
