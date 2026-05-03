4791f8f8534cf4db7dee5ad3ef5945b92186752f
Mlee671 <mitchwill.lee@gmail.com>
2026-05-04 11:29:20 +1200

    bugfix: fixed fountain going on cooldown due to text being visible at game start

--- Room_scenes/fountain.gd
+++ Room_scenes/fountain.gd
@@ -1,5 +1,6 @@
 extends StaticBody2D
 class_name fountain
+
 @onready var rich_text_label: RichTextLabel = $RichTextLabel
 @onready var explosion := $Explosion
 
@@ -11,9 +12,8 @@ func _on_interaction_zone_body_entered(body: Node2D) -> void:
 		
 
 func _on_interaction_zone_body_exited(body: Node2D) -> void:
-	pass
-	#if body is main_character:
-		#rich_text_label.visible = false
+	if body is main_character:
+		rich_text_label.visible = false
 
 func _physics_process(_delta: float) -> void:
 	if rich_text_label.visible and !used:
@@ -23,11 +23,11 @@ func _physics_process(_delta: float) -> void:
 			explosion.play()
 			SFX_Manager.play_sound_effect_from_dictionary("music_box_mystery")
 
-			used = true
+			self.used = true
 			Input.action_press("debug_open_reward_screen")
 			Input.action_release("debug_open_reward_screen")
 
-			rich_text_label.text = "\nPress 'E\nto change character\n"
+			self.rich_text_label.text = "\nPress 'E\nto change character\n"
 
 			
 func _on_explosion_animation_finished() -> void:
